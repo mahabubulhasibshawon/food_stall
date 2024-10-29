15d870c20412158107fef3b65c98c8668d97fa69
mahabubulhasibshawon <mahabubulhasibshawon@gmail.com>
2024-10-29 11:36:25 +0600

    header part

--- lib/screens/landing_screen.dart
+++ lib/screens/landing_screen.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:food_stall/utils/constants.dart';
+import 'package:food_stall/utils/widget_functions.dart';
 
 const PRODUCT_DATA = [
-  {"image": "pizza.png", "name": "Pizza", "rest": "Maritine Star Restaurant", "rating": "4.5 (164)", "price": 20, "currency": "\$"},
-  {"image": "burger.png", "name": "Burger", "rest": "Maritine Star Restaurant", "rating": "4.7 (199)", "price": 10, "currency": "\$"},
-  {"image": "fries.png", "name": "Fries", "rest": "Maritine Star Restaurant", "rating": "4.2 (101)", "price": 10, "currency": "\$"},
-  {"image": "hotdog.png", "name": "HotDog", "rest": "Maritine Star Restaurant", "rating": "3.9 (150)", "price": 15, "currency": "\$"},
+  {
+    "image": "pizza.png",
+    "name": "Pizza",
+    "rest": "Maritine Star Restaurant",
+    "rating": "4.5 (164)",
+    "price": 20,
+    "currency": "\$"
+  },
+  {
+    "image": "burger.png",
+    "name": "Burger",
+    "rest": "Maritine Star Restaurant",
+    "rating": "4.7 (199)",
+    "price": 10,
+    "currency": "\$"
+  },
+  {
+    "image": "fries.png",
+    "name": "Fries",
+    "rest": "Maritine Star Restaurant",
+    "rating": "4.2 (101)",
+    "price": 10,
+    "currency": "\$"
+  },
+  {
+    "image": "hotdog.png",
+    "name": "HotDog",
+    "rest": "Maritine Star Restaurant",
+    "rating": "3.9 (150)",
+    "price": 15,
+    "currency": "\$"
+  },
 ];
 
 const CATEGORIES = [
@@ -25,6 +55,88 @@ class LandingScreen extends StatefulWidget {
 class _LandingScreenState extends State<LandingScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final height = MediaQuery.sizeOf(context).height * 1;
+    final width = MediaQuery.sizeOf(context).width * 1;
+    final TextTheme textTheme = Theme.of(context).textTheme;
+    return SafeArea(
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: LayoutBuilder(builder: (context, constraints) {
+          return Container(
+            child: Column(
+              children: [
+                Expanded(
+                  flex: 4,
+                  child: Stack(
+                    fit: StackFit.expand,
+                    children: [
+                      Container(
+                        color: COLOR_GREEN,
+                      ),
+                      Image.asset('assets/images/tree_v.png'),
+                      Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Row(
+                              children: [
+                                Container(
+                                  width: width * .1,
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  child: Padding(
+                                    padding: EdgeInsets.all(08),
+                                    child:
+                                        Image.asset("assets/images/avatar.png"),
+                                  ),
+                                ),
+                                addHorizontalSpace(width * .02),
+                                Expanded(
+                                    child: Text(
+                                  'How Hungry are you Today?',
+                                  style: textTheme.titleLarge!
+                                      .apply(color: Colors.white),
+                                ))
+                              ],
+                            ),
+                            TextField(
+                              style: TextStyle(color: Colors.white),
+                              decoration: InputDecoration(
+                                hintText: 'Search for foods',
+                                hintStyle: TextStyle(color: Colors.white),
+                                border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+                                prefixIcon: Icon(Icons.search,color: Colors.white,),
+                                suffixIcon: Container(
+                                  height: 70,
+                                  width: 70,
+                                  decoration: BoxDecoration(
+                                    color: Colors.white24,borderRadius: BorderRadius.only(topRight: Radius.circular(20), bottomRight: Radius.circular(20)),
+                                  ),
+                                  child: Icon(Icons.menu, color: Colors.white,),
+                                ),
+                                fillColor: Colors.white24,
+                                filled: true,
+                              ),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  width: constraints.maxWidth,
+                  color: Colors.grey.shade200,
+                  height: height * .6,
+                )
+              ],
+            ),
+          );
+        }),
+      ),
+    );
   }
 }
